de6e3c97ad4a81f15df6b48acfd1e242d016e52c
Owen Buckley <owen.buckley@analogstudios.net>
2017-05-05 23:32:23 -0400

    first draft in progress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello!');  // eslint-disable-line
+#!/usr/bin/env node
+
+// console.log('__dirname!', __dirname);  // eslint-disable-line
+// console.log('process.cwd()!', process.cwd());  // eslint-disable-line
+
+const YarnLinter = require('./yarn-linter');
+const context = process.cwd();
+
+new YarnLinter(context);  // eslint-disable-now no-new
---
+++ src/yarn-linter.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+class YarnLinter {
+
+  constructor(context) {
+    this.fsOptions = {
+      encoding: 'utf-8'
+    };
+
+    if (this.validateContext(context)) {
+      this.validateYarnInstall(context);
+    }
+  }
+
+  validateContext(context  = null) {
+    if (!context){
+      throw new Error('no context provided!')
+    }
+
+    return true
+  }
+
+  validateYarnInstall(context){
+    // get files
+    const packageJson = require(`${context}/package.json`);
+    // const yarnLock = fs.readFileSync(`${context}/yarn.lock`, this.fsOptions);
+    const dependencies = this.getDependencies(packageJson);
+
+    // console.log('dependencies', dependencies);  // eslint-disable-line
+    return dependencies;
+  }
+
+  getDependencies(packageJson) {
+    const mergedDeps = {};
+    const deps = packageJson.peerDependencies || {};
+    const devDeps = packageJson.devDependencies || {};
+    const peerDeps = packageJson.dependencies || {};
+
+    console.log('deps', deps);  // eslint-disable-line
+    console.log('devDeps', devDeps);  // eslint-disable-line
+    console.log('peerDeps', peerDeps);  // eslint-disable-line
+
+    for (let key in deps) {
+      // console.log('!!!!!key', key); //eslint-disable-line
+      mergedDeps[key] = deps[key].replace('^', '').replace('~');
+    }
+
+    for (let key in devDeps) {
+      // console.log('!!!!!key', key); //eslint-disable-line
+      mergedDeps[key] = devDeps[key].replace('^', '').replace('~');
+    }
+
+    for (let key in peerDeps) {
+      // console.log('!!!!!key', key); //eslint-disable-line
+      mergedDeps[key] = peerDeps[key].replace('^', '').replace('~');
+    }
+
+    console.log('mergedDeps', mergedDeps); // eslint-disable-line
+    return mergedDeps;
+  }
+}
+
+module.exports = YarnLinter;
